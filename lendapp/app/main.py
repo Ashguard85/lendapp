d989800084f0c731ea125976e62a0033a0d57db1
Ashguard85 <philipp-haas@bluewin.ch>
2026-05-22 15:08:27 +0200

    Update main.py

--- lendapp/app/main.py
+++ lendapp/app/main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://192.168.1.127:3100"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 from app.database import engine, Base
 from app.routers import users, groups, items, bookings
 
