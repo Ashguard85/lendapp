c26a0e0988063acae469113702fefbdbba4e3280
Ashguard85 <philipp-haas@bluewin.ch>
2026-05-23 22:01:22 +0200

    Update main.py

--- lendapp/app/main.py
+++ lendapp/app/main.py
@@ -1,27 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from app.database import engine, Base
 from app.routers import users, groups, items, bookings
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="LendApp API",
     description="Gegenstände ausleihen mit Freunden & Familie",
     version="1.0.0",
+    root_path="/api",
 )
 
-# CORS NUR EINMAL
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://192.168.1.127:3100"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-Base.metadata.create_all(bind=engine)
 
 app.include_router(users.router,    prefix="/users",    tags=["Users"])
 app.include_router(groups.router,   prefix="/groups",   tags=["Groups"])
